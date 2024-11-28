22d40acab2dd1286c50c161d2c576db5344a6ff0
Brandon Trujillo <brandomwqe@hotmail.com>
2024-11-28 07:23:41 -0500

    actu

---
+++ js/proximamente.js
@@ -0,0 +1,30 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = 'January 31, 2025 00:00:00'; // Reemplaza con la fecha deseada
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// Actualiza la cuenta regresiva cada segundo
+setInterval(countdown, 1000);
